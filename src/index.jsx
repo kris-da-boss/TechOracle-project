4bcc1395e63932ebd1825dbdc7cc29f4eae2870c
Kris Wilosn <kriswilson00000@gmail.com>
2025-12-25 03:23:23 +0100

    fixed path

--- src/index.jsx
+++ src/index.jsx
@@ -6,8 +6,7 @@ import { BrowserRouter } from 'react-router'
 
 
 createRoot(document.getElementById('root')).render(
-  
-    <BrowserRouter>
+    <BrowserRouter basename='/TechOracle-project/'>
      <App />
     </BrowserRouter>  
 
